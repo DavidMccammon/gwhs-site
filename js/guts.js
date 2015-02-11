dc46183158c37b83351eb9420db1476a36461215
DavidMccammon <Dmcca3649@withs.org>
2015-02-11 09:33:20 -0500

    Create guts.js

---
+++ js/guts.js
@@ -0,0 +1,23 @@
+function divide(x, y) {
+	return x/y;
+}
+function multiply(x, y) {
+ return x*y;
+}
+function add(x, y) {
+	return x+y;
+}
+function subtract(x, y) {
+	return x-y;
+}
+function generateRandomNumber() {
+	 Math.random(x);	
+}
+function power(x, y) {
+	Math.pow(x,y);
+}
+function square_root(x) {
+	Math.sqrt(x);
+}
+function logBase10(x) {
+	Math.LOG10E;
